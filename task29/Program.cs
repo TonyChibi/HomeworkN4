2b31f7556b6f3153fd7847e135bd3bccb15ac39e
GlazHorsez <to5gogol@gmail.com>
2023-05-30 19:16:35 +0300

    add task N 29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿//не зашла задачка. без души писал
+Console.Clear();
+int[] arr=new int[8];
+Console.WriteLine("insert 8 integers: ");
+for (int i=0;i<arr.Length;i++){
+    Console.Write($"insert integer number {i}: ");
+    arr[i]=int.Parse(Console.ReadLine()!.ToString());
+}
+Console.WriteLine($"your array contains {string.Join(",",arr)} elements");
