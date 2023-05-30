bef0232ca8d12f5000e99ddce153a3b0b4b2d697
GlazHorsez <to5gogol@gmail.com>
2023-05-30 17:54:10 +0300

    add task N 25

---
+++ task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("insert an integer: ");
+int n=int.Parse(Console.ReadLine()!);
+Console.Write("insert a grade for the integer: ");
+int pow=int.Parse(Console.ReadLine()!);
+double output=n;
+for(int i=1;i<pow;i++)output*=n;
+Console.WriteLine(output);
